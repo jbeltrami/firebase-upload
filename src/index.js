8c1b4931ff5a10fc02f9bb470671b7d90ce1ac23
jbeltrami <joao_beltrami@hotmail.com>
2019-10-18 09:49:38 -0400

    Add redux, redux-firebase and redux-firestore to project.
    Use those libraries to create a store and a context provider to the app.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createFirestoreInstance } from 'redux-firestore';
+import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase';
+import firebase from 'firebase/app';
+import firebaseConfig from './config/fbConfig';
 import App from './App';
+import 'firebase/firestore';
+import 'firebase/auth';
+// Redux
+import rootReducer from './store/reducers/rootReducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  rootReducer,
+  compose(applyMiddleware(thunk.withExtraArgument(getFirebase)))
+);
+
+const rrfpProps = {
+  firebase,
+  config: {
+    ...firebaseConfig,
+    useFirestoreForProfile: true,
+    userProfile: 'users',
+  },
+  dispatch: store.dispatch,
+  createFirestoreInstance,
+};
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfpProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ src/store/reducers/authReducer.js
@@ -0,0 +1,11 @@
+const authReducer = (state = {}, action) => {
+  switch (action) {
+    case 'CREATE_USER':
+      return state;
+
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
